26cdb7bbe084762b6ec1ecfcecffd6ebbe8949f3
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-16 00:58:09 +0530

    [Dipti] added usercase-1 to find maximum between three numbers

---
+++ FindMaxUsingGenerics_CS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics_CS
+{/// <summary>
+/// Template for displaying maximum number between three num
+/// </summary>
+    class Program
+    {
+        //UC1 to display maximum between three integer
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The maximum value is:" + FindMaximum.FindMax(21,10,28));
+            Console.ReadLine();
+        }
+    }
+}
